0c5fde2123946e925ed2a2799d17e6d40517ab02
e1b22027 <e1b22027@oit.ac.jp>
2024-10-15 13:51:41 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/lec04/kadai/lec04/Lec04Application.java
@@ -0,0 +1,13 @@
+package oit.is.lec04.kadai.lec04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lec04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lec04Application.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/lec04/kadai/lec04/Lec04ApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.lec04.kadai.lec04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lec04ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
